ca8def671fda2b9a30457ca2a60bc7a30afbe43b
Joan Gerard <joan.gerard@outlook.com>
2023-03-03 16:28:40 +0100

    define Sanity product schema, configure Sanity project and write lambda to fetch sanity data

--- src/functions/cronJob/index.ts
+++ src/functions/cronJob/index.ts
@@ -1,6 +1,16 @@
+import { client } from '@libs/SanityClient';
 
+// export const handler = (event: any, context: any, callback) => {
+//   console.log('Hello, Welcome to Hevo');
+//   callback(null);
+// };
 
-export const handler = (event: any, context: any, callback) => {
-  console.log('Hello, Welcome to Hevo');
-  callback(null);
+export const handler = async () => {
+  console.log('CRON JOB START');
+  const productsQuery = '*[_type == "product"]';
+  const products: any[] = await client.fetch(productsQuery);
+
+  console.log({ products });
+  console.log('CRON ROB ENDED');
+
 };
